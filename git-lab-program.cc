07b2bfd3a5dd02c9ab27c8c43d9b030ad8582a12
Torin <mr001321@ohio.edu>
2022-01-19 17:46:28 -0500

    Work

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Torin T. Reid
+ *        Date: 1/19/22
+ * Description: Testing out github repos
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
